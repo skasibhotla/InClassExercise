5a7d71464e99acdb37ff91c83a12140fc22647c4
sirisha kasibhotla <skasibhotla@usf.edu>
2020-02-07 11:50:13 -0500

    working on Inclass exercise

---
+++ InClassExercise/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InClassExercise
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+        private Car(string modelName, string modelColor, int modelYear)
+        {
+            model = modelName;
+            color = modelColor;
+            year = modelYear;
+        }
+    }
+}
--- InClassExercise/Program.cs
+++ InClassExercise/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace InClassExercise
 {
@@ -6,7 +8,21 @@ namespace InClassExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Car Ford = new Car();
+            //Ford.model = "Mustang";
+            //Ford.color = "red";
+            //Ford.year = 1969;
+
+            //Car Opel = new Car();
+            //Opel.model = "Astra";
+            //Opel.color = "white";
+            //Opel.year = 2005;
+
+            Car Ford = new Car("Mustang", "Red", 1969);
+            Car Opel = new Car("Astra", "White", 2005);
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
         }
     }
 }
